c6b711b276f48abb49584829772039080b03efd8
Sahil Chaudhary <sahil280114@gmail.com>
2023-02-13 20:12:24 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,18 +1,14 @@
 # In this file, we define download_model
 # It runs during container build time to get model weights built into the container
 
-from diffusers import StableDiffusionUpscalePipeline
+from diffusers import StableDiffusionDepth2ImgPipeline
 import torch
 import os
 def download_model():
-    
-    s = os.getenv("dd")
-    print(s)
-    
     # do a dry run of loading the huggingface model, which will download weights at build time
-    #Set auth token which is required to download stable diffusion model weights
-    model_id = "stabilityai/stable-diffusion-x4-upscaler"
-    model = StableDiffusionUpscalePipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    
+    pipe = StableDiffusionDepth2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-2-depth",torch_dtype=torch.float16)
+    
 
 if __name__ == "__main__":
     download_model()
