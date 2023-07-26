fa9576a401be00568c9cd3862ddf7d5709ef97cc
kylejmorris <kyle@banana.dev>
2023-07-26 06:52:37 +0000

    migrate to v2. can do simple call local but don't have emb.pt weights so unable to test on proepr textual-inversion embeddings

--- app.py
+++ app.py
@@ -1,148 +1,34 @@
-import torch
-from diffusers import StableDiffusionDepth2ImgPipeline
-import base64
-from io import BytesIO
-from PIL import Image
-import requests
-from typing import Callable, Dict, List, Union
+# Do not edit if deploying to Banana Serverless
+# This file is boilerplate for the http server, and follows a strict interface.
 
-def extract_token_from_dict(embedding_dict: Dict[str, str]) -> str:
-        r"""
-        Extracts the token from the embedding dictionary.
+# Instead, edit the init() and inference() functions in app.py
 
-        Arguments:
-            embedding_dict (`Dict[str, str]`):
-                The embedding dictionary loaded from the embedding path
+from potassium import Potassium, Request, Response
+import subprocess
+import user_src 
 
-        Returns:
-            token (`str`):
-                The token to be added to the tokenizers' vocabulary
-        """
-        # auto1111 embedding case
-        if "string_to_param" in embedding_dict:
-            token = embedding_dict["name"]
-            return token
+# We do the model load-to-GPU step on server startup
+# so the model object is available globally for reuse
+app = Potassium("my_app")
 
-        return list(embedding_dict.keys())[0]
-
-def extract_embedding_from_dict(embedding_dict: Dict[str, str]) -> torch.Tensor:
-    r"""
-    Extracts the embedding from the embedding dictionary.
-    Arguments:
-        embedding_dict (`Dict[str, str]`):
-            The embedding dictionary loaded from the embedding path
-    Returns:
-        embedding (`torch.Tensor`):
-            The embedding to be added to the text encoder's embedding matrix
-    """
-    # auto1111 embedding case
-    if "string_to_param" in embedding_dict:
-        embedding_dict = embedding_dict["string_to_param"]
-        embedding = embedding_dict["*"]
-        return embedding
-    return list(embedding_dict.values())[0]
-
-def add_textual_inversion_embedding(model, token: str, embedding: torch.Tensor):
-        r"""
-        Adds a token to the tokenizer's vocabulary and an embedding to the text encoder's embedding matrix.
-
-        Arguments:
-            token (`str`):
-                The token to be added to the tokenizers' vocabulary
-            embedding (`torch.Tensor`):
-                The embedding of the token to be added to the text encoder's embedding matrix
-        """
-    
-
-        embedding = embedding.to(model.text_encoder.dtype)
-
-        if token in model.tokenizer.get_vocab():
-            # If user has allowed replacement and the token exists, we only need to
-            # extract the existing id and update the embedding
-            token_id = model.tokenizer.convert_tokens_to_ids(token)
-            model.text_encoder.get_input_embeddings().weight.data[token_id] = embedding
-            return model
-        else:
-            # If the token does not exist, we add it to the tokenizer, then resize and update the
-            # text encoder acccordingly
-            model.tokenizer.add_tokens([token])
-
-            token_id = model.tokenizer.convert_tokens_to_ids(token)
-            # NOTE: len() does't start at 0, so we shouldn't need to +1
-            # since we already updated the tokenizer and it's new length
-            # should be old length + 1
-            model.text_encoder.resize_token_embeddings(len(model.tokenizer))
-            model.text_encoder.get_input_embeddings().weight.data[token_id] = embedding
-            return model
-
-# Init is ran on server startup
-# Load your model to GPU as a global variable here using the variable name "model"
+# @app.init runs at startup, and loads models into the app's context
+@app.init
 def init():
-    global model
-    
-    
-    model = StableDiffusionDepth2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-2-depth",torch_dtype=torch.float16).to("cuda")
-    textual_inversion_embedding = torch.load("emb.pt",map_location="cuda")
-    token = extract_token_from_dict(textual_inversion_embedding)
-    embedding = extract_embedding_from_dict(textual_inversion_embedding)
-    embedding = embedding[0]
-    model = add_textual_inversion_embedding(model,token,embedding)
+    context = user_src.init()
+    return context
 
-# Inference is ran for every server call
-# Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
-    global model
+# Inference POST handler at '/' is called for every http call from Banana
+@app.handler('/')
+def inference(context: dict, request: Request) -> Response:
+    model_inputs =  request.json
+    model = context.get('model')
 
-    # Parse out your arguments
-    image_url = model_inputs.get('imageURL', None)
-    if image_url == None:
-        return {'message': "No image url provided"}
-    
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
+    output = user_src.inference(model, model_inputs)
 
-    n_prompt = model_inputs.get('negative_prompt', "")
-    strength = model_inputs.get('strength', 0.8)
-    num_inference_steps = model_inputs.get('num_inference_steps', 50)
-    guidance_scale = model_inputs.get('guidance_scale', 7.5)
+    return Response(
+        json = {"outputs": output}, 
+        status=200
+    )
 
-
-    print(model_inputs.get('imageURL', None))
-    print(model_inputs.get('strength', 0.7))
-    print(model_inputs.get('prompt', None))
-    print(model_inputs.get('negative_prompt', ""))
-
-    init_image = Image.open(requests.get(image_url, stream=True).raw)
-
-#   output_image = pipe(prompt=prompt, image=init_image).images[0]
-#   output_images = model(prompt=prompt, image=init_image, num_images_per_prompt=4)
-
-    output_images = model(prompt=prompt, image=init_image, num_images_per_prompt=4, negative_prompt=n_prompt, strength=strength, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images
- 
-    output_image = output_images[0]
-
-    buffered = BytesIO()
-    output_image.save(buffered,format="JPEG")
-    image_base64_0 = base64.b64encode(buffered.getvalue()).decode('utf-8')
-
-    output_image = output_images[1]
-
-    buffered = BytesIO()
-    output_image.save(buffered,format="JPEG")
-    image_base64_1 = base64.b64encode(buffered.getvalue()).decode('utf-8')
-    output_image = output_images[2]
-
-    buffered = BytesIO()
-    output_image.save(buffered,format="JPEG")
-    image_base64_2 = base64.b64encode(buffered.getvalue()).decode('utf-8')
-
-    output_image = output_images[3]
-    
-    buffered = BytesIO()
-    output_image.save(buffered,format="JPEG")
-    image_base64_3 = base64.b64encode(buffered.getvalue()).decode('utf-8')
-
-        
-    # Return the results as a dictionary
-    return {'image_base64_0': image_base64_0, 'image_base6_1': image_base64_1, 'image_base64_2': image_base64_2, 'image_base64_3': image_base64_3}
+if __name__ == '__main__':
+    app.serve()
---
+++ user_src.py
@@ -0,0 +1,145 @@
+import torch
+from diffusers import StableDiffusionDepth2ImgPipeline
+import base64
+from io import BytesIO
+from PIL import Image
+import requests
+from typing import Callable, Dict, List, Union
+
+def extract_token_from_dict(embedding_dict: Dict[str, str]) -> str:
+        r"""
+        Extracts the token from the embedding dictionary.
+
+        Arguments:
+            embedding_dict (`Dict[str, str]`):
+                The embedding dictionary loaded from the embedding path
+
+        Returns:
+            token (`str`):
+                The token to be added to the tokenizers' vocabulary
+        """
+        # auto1111 embedding case
+        if "string_to_param" in embedding_dict:
+            token = embedding_dict["name"]
+            return token
+
+        return list(embedding_dict.keys())[0]
+
+def extract_embedding_from_dict(embedding_dict: Dict[str, str]) -> torch.Tensor:
+    r"""
+    Extracts the embedding from the embedding dictionary.
+    Arguments:
+        embedding_dict (`Dict[str, str]`):
+            The embedding dictionary loaded from the embedding path
+    Returns:
+        embedding (`torch.Tensor`):
+            The embedding to be added to the text encoder's embedding matrix
+    """
+    # auto1111 embedding case
+    if "string_to_param" in embedding_dict:
+        embedding_dict = embedding_dict["string_to_param"]
+        embedding = embedding_dict["*"]
+        return embedding
+    return list(embedding_dict.values())[0]
+
+def add_textual_inversion_embedding(model, token: str, embedding: torch.Tensor):
+        r"""
+        Adds a token to the tokenizer's vocabulary and an embedding to the text encoder's embedding matrix.
+
+        Arguments:
+            token (`str`):
+                The token to be added to the tokenizers' vocabulary
+            embedding (`torch.Tensor`):
+                The embedding of the token to be added to the text encoder's embedding matrix
+        """
+    
+
+        embedding = embedding.to(model.text_encoder.dtype)
+
+        if token in model.tokenizer.get_vocab():
+            # If user has allowed replacement and the token exists, we only need to
+            # extract the existing id and update the embedding
+            token_id = model.tokenizer.convert_tokens_to_ids(token)
+            model.text_encoder.get_input_embeddings().weight.data[token_id] = embedding
+            return model
+        else:
+            # If the token does not exist, we add it to the tokenizer, then resize and update the
+            # text encoder acccordingly
+            model.tokenizer.add_tokens([token])
+
+            token_id = model.tokenizer.convert_tokens_to_ids(token)
+            # NOTE: len() does't start at 0, so we shouldn't need to +1
+            # since we already updated the tokenizer and it's new length
+            # should be old length + 1
+            model.text_encoder.resize_token_embeddings(len(model.tokenizer))
+            model.text_encoder.get_input_embeddings().weight.data[token_id] = embedding
+            return model
+
+# Init is ran on server startup
+# Load your model to GPU as a global variable here using the variable name "model"
+def init():
+    model = StableDiffusionDepth2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-2-depth",torch_dtype=torch.float16).to("cuda")
+    textual_inversion_embedding = torch.load("emb.pt",map_location="cuda")
+    token = extract_token_from_dict(textual_inversion_embedding)
+    embedding = extract_embedding_from_dict(textual_inversion_embedding)
+    embedding = embedding[0]
+    model = add_textual_inversion_embedding(model,token,embedding)
+
+    return {'model': model}
+
+# Inference is ran for every server call
+# Reference your preloaded global model variable here.
+def inference(model, model_inputs) -> dict:
+    # Parse out your arguments
+    image_url = model_inputs.get('imageURL', None)
+    if image_url == None:
+        return {'message': "No image url provided"}
+    
+    prompt = model_inputs.get('prompt', None)
+    if prompt == None:
+        return {'message': "No prompt provided"}
+
+    n_prompt = model_inputs.get('negative_prompt', "")
+    strength = model_inputs.get('strength', 0.8)
+    num_inference_steps = model_inputs.get('num_inference_steps', 50)
+    guidance_scale = model_inputs.get('guidance_scale', 7.5)
+
+
+    print(model_inputs.get('imageURL', None))
+    print(model_inputs.get('strength', 0.7))
+    print(model_inputs.get('prompt', None))
+    print(model_inputs.get('negative_prompt', ""))
+
+    init_image = Image.open(requests.get(image_url, stream=True).raw)
+
+#   output_image = pipe(prompt=prompt, image=init_image).images[0]
+#   output_images = model(prompt=prompt, image=init_image, num_images_per_prompt=4)
+
+    output_images = model(prompt=prompt, image=init_image, num_images_per_prompt=4, negative_prompt=n_prompt, strength=strength, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images
+ 
+    output_image = output_images[0]
+
+    buffered = BytesIO()
+    output_image.save(buffered,format="JPEG")
+    image_base64_0 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+    output_image = output_images[1]
+
+    buffered = BytesIO()
+    output_image.save(buffered,format="JPEG")
+    image_base64_1 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    output_image = output_images[2]
+
+    buffered = BytesIO()
+    output_image.save(buffered,format="JPEG")
+    image_base64_2 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+    output_image = output_images[3]
+    
+    buffered = BytesIO()
+    output_image.save(buffered,format="JPEG")
+    image_base64_3 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+        
+    # Return the results as a dictionary
+    return {'image_base64_0': image_base64_0, 'image_base6_1': image_base64_1, 'image_base64_2': image_base64_2, 'image_base64_3': image_base64_3}
